c433f841ccc84c8af7d0db72ea3bb276c7863b3b
sahilhawal <sahilhawal1996@gmail.com>
2020-05-09 11:26:13 +0530

    Dice-game Completed

--- Dice-Game/app.js
+++ Dice-Game/app.js
@@ -8,27 +8,67 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-let activePlayer, playerOneScore, playerTwoScore;
+let activePlayer,
+  playerOneScore,
+  playerTwoScore,
+  playerOneScoreCurrent,
+  playerTwoScoreCurrent;
 
-(function () {
+const newGame = () => {
   activePlayer = "playerOne";
   playerOneScore = 0;
   playerTwoScore = 0;
-})();
+  playerOneScoreCurrent = 0;
+  playerTwoScoreCurrent = 0;
+  const scoreElements = document.getElementsByClassName("player-score");
+  const scoreElementsCurrent = document.getElementsByClassName(
+    "player-current-score"
+  );
+  for (var i = 0; i < scoreElements.length; i++) {
+    scoreElements[i].innerHTML = 0;
+  }
+  for (var i = 0; i < scoreElementsCurrent.length; i++) {
+    scoreElementsCurrent[i].innerHTML = 0;
+  }
+};
+
+newGame();
 
 document.querySelector(".btn-roll").addEventListener("click", function () {
   const diceNumber = Math.floor(Math.random() * 6 + 1);
-  console.log(diceNumber);
   document.querySelector(".dice").src = "dice-" + diceNumber + ".png";
 
   if (diceNumber === 1) {
-    eval(`${activePlayer}Score = 0`);
+    eval(`${activePlayer}ScoreCurrent = 0`);
+    document.getElementById(`${activePlayer}ScoreCurrent`).innerHTML = 0;
   } else {
-    eval(`${activePlayer}Score += ${diceNumber}`);
+    eval(`${activePlayer}ScoreCurrent += ${diceNumber}`);
+    document.getElementById(`${activePlayer}ScoreCurrent`).innerHTML = eval(
+      activePlayer + "ScoreCurrent"
+    );
   }
-  console.log(activePlayer, playerOneScore, playerTwoScore);
+  console.log(
+    `${activePlayer}ScoreCurrent`,
+    activePlayer,
+    playerOneScore,
+    playerTwoScore,
+    playerOneScoreCurrent,
+    playerTwoScoreCurrent
+  );
+  if (eval(`${activePlayer}Score`) >= 50) {
+    alert(activePlayer == "playerOne" ? "Player 1 Wins" : "Player 2 Wins");
+    console.log(`${activePlayer} wins`);
+  }
 });
 
 document.querySelector(".btn-hold").addEventListener("click", function () {
+  eval(`${activePlayer}Score += ${activePlayer}ScoreCurrent`);
+  eval(`${activePlayer}ScoreCurrent=0`);
+  document.getElementById(`${activePlayer}ScoreCurrent`).innerHTML = 0;
+  document.getElementById(`${activePlayer}Score`).innerHTML = eval(
+    `${activePlayer}Score`
+  );
   activePlayer = activePlayer == "playerOne" ? "playerTwo" : "playerOne";
 });
+
+document.querySelector(".btn-new").addEventListener("click", newGame);
